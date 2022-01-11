ed9fb677aea748c00bcd35c146cc52abd720b281
MBPhys <marc.boucsein@gmail.com>
2022-01-11 16:25:39 +0100

    Image-Part-Selecter

---
+++ Image_Part_Selecter/__init__.py
@@ -0,0 +1,12 @@
+from .image_select import napari_experimental_provide_dock_widget
+
+#__author__ = "Viktor van der Valk"
+#__email__ = "v.o.van_der_valk@lumc.nl"
+
+__version__ = "0.1.7"
+
+
+def get_module_version():
+    return __version__
+
+
